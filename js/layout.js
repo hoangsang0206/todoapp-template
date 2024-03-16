c30cd58f29de0ab0f8f379e76f9ffff52825c003
Sang <lehoangsang02062003@gmail.com>
2024-03-16 15:13:55 +0700

    Update layout.html

--- js/layout.js
+++ js/layout.js
@@ -9,4 +9,31 @@ $('.nav-link').click(function() {
 
 $('.toggle-nav').click(() => {
     $('.navigation').toggleClass('close');
+    setMainWidth();
 })
+
+$('.add-new-task').click(() => {
+    $('.task-infomation-wrapper').removeClass('close');
+    setMainWidth();
+})
+
+$('.toggle-task-info').click(() => {
+    $('.task-infomation-wrapper').toggleClass('close');
+    setMainWidth();
+})
+
+function setMainWidth() {
+    let main = $('main');
+    let taskInf = $('.task-infomation-wrapper');
+    let nav = $('.navigation');
+
+    if(!taskInf.hasClass('close') && !nav.hasClass('close')) {  
+        main.css('width', 'calc(100% - 570px)');
+    } else if(!nav.hasClass('close') && taskInf.hasClass('close')) {
+        main.css('width', 'calc(100% - 270px)');
+    } else if(nav.hasClass('close') && !taskInf.hasClass('close')) {
+        main.css('width', 'calc(100% - 370px)');
+    } else {
+        main.css('width', 'calc(100% - 70px)');
+    }
+}
