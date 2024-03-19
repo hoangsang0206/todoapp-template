6e7df2b5d842cef07d5798f9c5268dd0a28fa0ae
Sang <lehoangsang02062003@gmail.com>
2024-03-19 16:11:06 +0700

    Update layout.html/index.html

--- js/layout.js
+++ js/layout.js
@@ -1,5 +1,49 @@
+function setMainWidth() {
+    if(window.innerWidth >= 1024) {
+        let main = $('main');
+        let taskInf = $('.task-infomation-wrapper');
+        let nav = $('.navigation');
+
+        if(!taskInf.hasClass('close') && !nav.hasClass('close')) {  
+            main.css('width', 'calc(100% - 570px)');
+        } else if(!nav.hasClass('close') && taskInf.hasClass('close')) {
+            main.css('width', 'calc(100% - 270px)');
+        } else if(nav.hasClass('close') && !taskInf.hasClass('close')) {
+            main.css('width', 'calc(100% - 370px)');
+        } else {
+            main.css('width', 'calc(100% - 70px)');
+        }
+    }
+}
+
+function showOverlay() {
+    if(window.innerWidth < 1024) {
+        $('.overlay').show();
+    } else {
+        $('.overlay').hide();
+    }
+}
+
+function hideOverlay() {
+    $('.overlay').hide();
+}
+
+function showContentLoading() {
+    $('.loading').css('display', 'grid');
+}
+
+function hideContentLoading() {
+    $('.loading').hide();
+}
+
 $(window).on('load', () => {
     $('.page-loader').removeClass('show');
+})
+
+$(window).resize(() => {
+    if(window.innerWidth < 768) {
+        $('.navigation').removeClass('close');
+    }
 })
 
 $('.nav-link').click(function() {
@@ -8,32 +52,35 @@ $('.nav-link').click(function() {
 })
 
 $('.toggle-nav').click(() => {
-    $('.navigation').toggleClass('close');
-    setMainWidth();
+    if(window.innerWidth < 768) {
+        $('.navigation').removeClass('show');
+        hideOverlay();
+    } else {
+        $('.navigation').toggleClass('close');
+        setMainWidth();
+    }
 })
 
 $('.add-new-task').click(() => {
     $('.task-infomation-wrapper').removeClass('close');
     setMainWidth();
+    showOverlay();
 })
 
-$('.toggle-task-info').click(() => {
+$('.close-task-info').click(() => {
     $('.task-infomation-wrapper').toggleClass('close');
     setMainWidth();
+    hideOverlay();
 })
 
-function setMainWidth() {
-    let main = $('main');
-    let taskInf = $('.task-infomation-wrapper');
-    let nav = $('.navigation');
+$('.overlay').click(() => {
+    $('.navigation').removeClass('show');
+    $('.task-infomation-wrapper').addClass('close');
+    hideOverlay();
+})
 
-    if(!taskInf.hasClass('close') && !nav.hasClass('close')) {  
-        main.css('width', 'calc(100% - 570px)');
-    } else if(!nav.hasClass('close') && taskInf.hasClass('close')) {
-        main.css('width', 'calc(100% - 270px)');
-    } else if(nav.hasClass('close') && !taskInf.hasClass('close')) {
-        main.css('width', 'calc(100% - 370px)');
-    } else {
-        main.css('width', 'calc(100% - 70px)');
-    }
-}
+//Click to show navigation (mobile)
+$('.show-nav-action').click(() => {
+    $('.navigation').addClass('show');
+    showOverlay();
+})
